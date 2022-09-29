7eb658389b104116f4d2cea91b105d32e232d25b
Swathi Manoharan <swathi4721096@gmail.com>
2022-09-29 11:56:29 +0530

    First Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,22 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'machinetest';
+  public toDoList:any = [] ;
+  public isEditable = true;
+
+  addList(item: string){
+  
+    this.toDoList.push({id:item.length,name:item, newName:[]})
+    console.log(this.toDoList);    
+  }
+  removeTask(id: number){ 
+       
+    this.toDoList= this.toDoList.filter((item:any) =>item.id !== id)
+  }  
+  editTask(id:any){  
+  this.isEditable;
+  
+  }
+
+
 }
